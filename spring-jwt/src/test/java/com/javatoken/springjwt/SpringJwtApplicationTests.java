d20057cdf30aaa6c7935dd8c77ee38d66c3aee7c
jinwoov <jinwoov@gmail.com>
2021-02-12 16:25:58 -0600

    test

--- spring-jwt/src/test/java/com/javatoken/springjwt/SpringJwtApplicationTests.java
+++ spring-jwt/src/test/java/com/javatoken/springjwt/SpringJwtApplicationTests.java
@@ -1,13 +1,15 @@
 package com.javatoken.springjwt;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(features ="src/test/resources", plugin = {"pretty"})
 @SpringBootTest
 class SpringJwtApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
--- spring-jwt/src/test/java/com/javatoken/springjwt/runRouteTest.java
+++ spring-jwt/src/test/java/com/javatoken/springjwt/runRouteTest.java
@@ -3,8 +3,11 @@ package com.javatoken.springjwt;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 @RunWith(Cucumber.class)
-@CucumberOptions(features ="src/test/resources", plugin = {"pretty"})
+@CucumberOptions(features ="src/test/resources", plugin = {"pretty"},
+        glue = "com.testing")
 public class runRouteTest {
 }
