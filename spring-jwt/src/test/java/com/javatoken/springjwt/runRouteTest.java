9cb66fa04a25b0e416e0721de97beba63d9573ad
jinwoov <jinwoov@gmail.com>
2021-02-12 22:18:11 -0600

    fixed

--- spring-jwt/src/test/java/com/javatoken/springjwt/runRouteTest.java
+++ spring-jwt/src/test/java/com/javatoken/springjwt/runRouteTest.java
@@ -3,11 +3,8 @@ package com.javatoken.springjwt;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 @RunWith(Cucumber.class)
-@CucumberOptions(features ="src/test/resources", plugin = {"pretty"},
-        glue = "com.testing")
+@CucumberOptions(features ="src/test/resources", plugin = {"pretty"})
 public class runRouteTest {
 }
